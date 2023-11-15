e4b650f9d8ea2bed3ab872300fb4a358ff898b92
John1on <iancoion1@gmail.com>
2023-11-15 14:41:00 +0200

    Fix bug

--- Lesion 2. Hello World/ViewController.swift
+++ Lesion 2. Hello World/ViewController.swift
@@ -7,43 +7,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     @IBOutlet var redView: UIView!
     @IBOutlet var yellowView: UIView!
     @IBOutlet var greenView: UIView!
+    
     @IBOutlet var showColorButton: UIButton!
     
-    var currentSignalIndex = 0
-
-    private var semaphoreColors: [UIView] = []
-    private let cornerRadius: CGFloat = 50
+    private var currentLight = CurrentLight.red
+    private let lightIsOn = 1.0
+    private let lightIsOff = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redView.layer.cornerRadius = cornerRadius
-        yellowView.layer.cornerRadius = cornerRadius
-        greenView.layer.cornerRadius = cornerRadius
-        
         showColorButton.layer.cornerRadius = 25
         
-        semaphoreColors = [redView, yellowView, greenView]
-        semaphoreColors.forEach { $0.alpha = 0.3 }
+        redView.alpha = lightIsOff
+        yellowView.alpha = lightIsOff
+        greenView.alpha = lightIsOff
+        
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenView.layer.cornerRadius = greenView.frame.width / 2
+        
     }
     
     @IBAction func showCollorsDidTapped(_ sender: UIButton) {
-        // Turn off all signals at the beginning
-        semaphoreColors.forEach { $0.alpha = 0.3 }
+        if showColorButton.currentTitle == "START" {
+            showColorButton.setTitle("NEXT", for: .normal)
+        }
         
-        // Turn on the first signal when the button is pressed
-        semaphoreColors[currentSignalIndex].alpha = 1.0
-        
-        // Toggle through the signals starting from the first signal
-        currentSignalIndex = (currentSignalIndex + 1) % semaphoreColors.count
-        
-    
-        sender.setTitle("NEXT", for: .normal)
+        switch currentLight {
+        case .red:
+            greenView.alpha = lightIsOff
+            redView.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redView.alpha = lightIsOff
+            yellowView.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowView.alpha = lightIsOff
+            greenView.alpha = lightIsOn
+            currentLight = .red
+        }
     }
 }
 
+
+
+// MARK: - CurrentLight
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
+}
